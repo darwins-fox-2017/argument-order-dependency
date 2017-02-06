212f1656a19c1e5f49b56714b5b8d48edb08105a
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-06 11:37:25 +0700

    Done all release

--- house.js
+++ house.js
@@ -2,20 +2,21 @@
 
 class House {
 
-  constructor (address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor (component) {
+
+    this.address = component['address'] //address
+    this.square_feet = component['square_feet'] //square_feet
+    this.num_bedrooms = component['num_bedrooms'] || 3 //num_bedrooms || 3
+    this.num_baths = component['num_baths'] || 2 //num_baths || 2
+    this.cost = component['cost'] //cost || 320000
+    this.down_payment = component['down_payment'] || 0.20 //down_payment || 0.20
+    this.sold = component['sold'] || false //sold || false
+    this.short_sale = component['short_sale'] //short_sale
+    this.has_tenants = component['has_tenants'] || false//has_tenants || false
   }
 
   obscure_address () {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy (money, good_credit) {
@@ -33,7 +34,8 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+const data = {'address': 'Jalan Tanah Kusir II Kebayoran', 'square_feet': 100, 'num_bedrooms': 2, 'num_baths': 2, 'cost': 135000, 'down_payment': 0.15}
+
+const cool = new House(data)
 
 console.log(cool.to_s())
-
