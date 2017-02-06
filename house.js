28b97e4df13d9212639e7915a6a97aa382cbf798
irwinpratajaya <irwinpratajaya@gmail.com>
2017-02-06 11:58:03 +0700

    done

--- house.js
+++ house.js
@@ -2,16 +2,16 @@
 
 class House {
 
-  constructor (argument) {
-    this.address = argument['address']
-    this.square_feet = argument['square_feet']
-    this.num_bedrooms = argument['num_bedrooms'] || 3
-    this.num_baths = argument['num_baths'] || 2
-    this.cost = argument['cost'] || 320000
-    this.down_payment = argument['down_payment'] || 0.20
-    this.sold = argument['sold'] || false
-    this.short_sale = argument['short_sale']
-    this.has_tenants = argument['has_tenants'] || false
+  constructor (object) {
+    this.address = object['address']
+    this.square_feet = object['square_feet']
+    this.num_bedrooms = object['num_bedrooms'] || 3
+    this.num_baths = object['num_baths'] || 2
+    this.cost = object['cost'] || 320000
+    this.down_payment = object['down_payment'] || 0.20
+    this.sold = object['sold'] || false
+    this.short_sale = object['short_sale']
+    this.has_tenants = object['has_tenants'] || false
   }
 
   obscure_address () {
