907500d7c3443da1b744f110a8768a3fa673efe6
samuelphsinaga <samuelphsinaga@gmail.com>
2017-02-06 13:50:04 +0700

    arg order dependency

--- house.js
+++ house.js
@@ -2,38 +2,50 @@
 
 class House {
 
-  constructor (address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor (rumah) {
+    this.address = rumah['address']
+    this.square_feet = rumah['square_feet']
+    this.num_bedrooms = rumah['num_bedrooms'] || 3
+    this.num_baths = rumah['num_baths'] || 2
+    this.cost = rumah['cost'] || 320000
+    this.down_payment = rumah['down_payment'] || 0.20
+    this.sold = rumah['sold'] || false
+    this.short_sale = rumah['short_sale']
+    this.has_tenants = rumah['has_tenants'] || false
   }
 
   obscure_address () {
-    this.address.replace(/.{10}$/g, '****')
-  }
-
-  buy (money, good_credit) {
-    if (money >= down_payment && good_credit) {
-      this.sold = true
+      return this.address.replace(/.{10}$/g, '****')
     }
+
+    buy (money, good_credit) {
+      if (money >= down_payment && good_credit) {
+        this.sold = true
+      }
+    }
+
+    down_payment () {
+      return cost * this.downPayment
+    }
+
+    to_s () {
+ return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`    }
   }
 
-  down_payment () {
-    return cost * this.down_payment
+  let rincian = {
+    address: "Jalan Cisitu no 45b, Dago, Coblong, Kota Bandung, Jawa Barat 40135",
+    square_feet: 100,
+    num_bedrooms: 3,
+    num_baths: 2,
+    cost: 100000,
+    down_payment: 500000,
+    sold: false,
+    short_sale : 'for sale',
+    has_tenants: true
   }
 
-  to_s () {
-    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
-  }
-}
+  const pertama = new House(rincian)
+  console.log(pertama.address);
+  console.log(pertama.to_s() +"\n");
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
-
-console.log(cool.to_s())
-
+  console.log(pertama)
