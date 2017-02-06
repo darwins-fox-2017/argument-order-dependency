58010bf6bd6b231071d8882bf4457a82f0bf45a2
idbmb <bmb.router@gmail.com>
2017-02-06 14:25:11 +0700

    done argument order depency

--- house.js
+++ house.js
@@ -2,20 +2,23 @@
 
 class House {
 
-  constructor (address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor (argument) {
+  // (address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants)
+    this.address = argument ['address']
+    this.square_feet = argument ['square_feet']
+    this.num_bedrooms = argument ['num_bedrooms'] || 3
+    this.num_baths = argument ['num_baths'] || 2
+    this.cost = argument ['cost'] || 320000
+    this.down_payment = argument ['down_payment'] || 0.20
+    this.sold = argument ['sold'] || false
+    this.short_sale = argument ['short_sale']
+    this.has_tenants = argument ['has_tenants'] || false
   }
 
   obscure_address () {
-    this.address.replace(/.{10}$/g, '****')
+    // this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
+
   }
 
   buy (money, good_credit) {
@@ -29,11 +32,29 @@ class House {
   }
 
   to_s () {
-    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
+    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. $ ${this.cost}`
+    // return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
   }
 }
 
+// const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+//
+// console.log(cool.to_s())
+
+// beta test
+
+let argumentHome = {
+  address : "Jalan Dasa Raya No.7 Jakarta Selatan",
+  square_feet : 20,
+  num_bedrooms : 3,
+  num_baths : 2,
+  cost : 320000,
+  down_payment : 0.20,
+  sold : false,
+  short_sale : true,
+  has_tenants : false
+}
+
+let home = new House(argumentHome)
 const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
-
-console.log(cool.to_s())
-
+console.log(home.to_s())
